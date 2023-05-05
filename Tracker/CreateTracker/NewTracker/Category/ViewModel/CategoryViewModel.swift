c7fe609a6cb92a2c6248a2ff2a01800615f780bd
MaxCode2023 <course.ios@artsofte.ru>
2023-05-05 12:51:05 +0500

    view model impl

--- Tracker/CreateTracker/NewTracker/Category/ViewModel/CategoryViewModel.swift
+++ Tracker/CreateTracker/NewTracker/Category/ViewModel/CategoryViewModel.swift
@@ -7,7 +7,55 @@
 
 import Foundation
 
-final class CategoryViewModel {
+protocol CategoryViewModelDelegate {
+    func categoryListOnChange()
+    func didSelectCategory(category: TrackerCategory)
+}
+
+final class CategoryViewModel: TrackerCategoryStoreDelegate {
+    
+    
     private let trackerCategoryStore = TrackerCategoryStore()
+    weak var delegate: CategoryViewModelDelegate?
+    
+    init() {
+        trackerCategoryStore.delegate = self
+    }
+    
+    
+    private(set) var categoryList: [TrackerCategory]? {
+        didSet {
+            delegate?.categoryListOnChange()
+        }
+    }
+    
+    private(set) var selectedCategory: TrackerCategory? = nil {
+        didSet {
+            guard let selectedCategory else {return}
+            delegate?.didSelectCategory(category: selectedCategory)
+        }
+    }
+    
+    private func getDataFromStore() -> [TrackerCategory] {
+        do {
+            let categories = try trackerCategoryStore.categoriesCoreData.map({
+                try trackerCategoryStore.makeCategory(from: $0)
+            })
+            return categories
+        } catch {
+            return []
+        }
+        
+    }
+    
+    func selectCategoryCell(index: IndexPath) {
+        selectedCategory = categoryList?[index.row]
+    }
+    
+    
+    
+    func didUpdate() {
+        categoryList = getDataFromStore()
+    }
     
 }
